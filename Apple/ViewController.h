9ba8d6ed5aefcd15dc96bcb6782e94173574b2af
Leo <Leo@Leos-Macbook-Pro.local>
2017-05-04 16:57:38 +0530

    Update

---
+++ Apple/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  Apple
+//
+//  Created by SILICON on 03/09/16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "NSLayoutConstraint+Multiplier.h"
+
+@interface ViewController : UIViewController
+
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *layoutConstraintViewHeight;
+
+@end
+
